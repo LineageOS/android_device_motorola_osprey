85fc44c2bdfcd851c37de0d73b59cb5815a396f6
Benjamin Legrand <dev@legrand.ws>
2015-06-07 01:00:43 +0200

    This will allow building the libOmxAacEnc.so and some other libs
    Change-Id: Icb9547fb8b22130d1a9e8eb6d55d65a1446419da
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,8 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
+TARGET_USES_QCOM_MM_AUDIO := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
