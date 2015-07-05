ac588dc6d28e5ff9cf12e08c3e94fbf9a8cf036a
Benjamin Legrand <dev@legrand.ws>
2015-07-05 21:47:30 +0200

    Try to fix booting issue
    Change-Id: I633f4b0861a53f55f5671d281161fa3a35a5c0c0

--- rootdir/init.surnia.sh
+++ rootdir/init.surnia.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
 PATH=/system/bin
+TAG="scritch007"
 
 product_model=`getprop ro.product.model`
 
@@ -7,14 +8,14 @@ if [ "XT1526" = $product_model ]; then
 	log -t$TAG "Applying LTE fix"
 	setprop ro.cdma.home.operator.numeric 311870
 	setprop ro.cdma.home.operator.alpha "Boost Mobile"
-	setprop ro.product.locale.region US
+	setprop ro.product.locale.region "US"
 	setprop gsm.sim.operator.alpha "Boost Mobile"
 	setprop gsm.sim.operator.numeric 311870
 	setprop gsm.operator.alpha "Boost Mobile"
 	setprop gsm.operator.numeric 311870
-	setprop gsm.sim.operator.iso-country US
-	setprop gsm.operator.iso-country US
-	setprop ro.telephony.default_network 8
+	setprop gsm.sim.operator.iso-country "US"
+	#setprop "gsm.operator.iso-country" "US"
+	#setprop ro.telephony.default_network 8
 	#Now copy the eri.xml to /data/eri.xml
 	cp /system/etc/boost/eri.xml /data/eri.xml
 fi
