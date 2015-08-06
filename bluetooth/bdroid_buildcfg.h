31e3d96791de52a510ce4f565892538d5ff7fa54
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-08-05 21:56:02 -0400

    osprey: update bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Motorola Moto E 2015"
+#define BTM_DEF_LOCAL_NAME   "Motorola Moto G"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
