4b4545a1ed0f177bbe4ddaae7b8ebd4424e288fc
Alberto97 <albertop2197@gmail.com>
2015-08-07 15:44:02 +0200

    rootdir: First Cleanup
    Change-Id: I245c440b0c80dee78796c54d9e7dff1ab4038541

--- rootdir/init.mmi.boot.sh
+++ rootdir/init.mmi.boot.sh
@@ -119,3 +119,16 @@ then
 	setprop ro.mot.hw.fingerprint 1
 fi
 
+# Let kernel know our image version/variant/crm_version
+image_version="10:"
+image_version+=`getprop ro.build.id`
+image_version+=":"
+image_version+=`getprop ro.build.version.incremental`
+image_variant=`getprop ro.product.name`
+image_variant+="-"
+image_variant+=`getprop ro.build.type`
+oem_version=`getprop ro.build.version.codename`
+echo 10 > /sys/devices/soc0/select_image
+echo $image_version > /sys/devices/soc0/image_version
+echo $image_variant > /sys/devices/soc0/image_variant
+echo $oem_version > /sys/devices/soc0/image_crm_version
