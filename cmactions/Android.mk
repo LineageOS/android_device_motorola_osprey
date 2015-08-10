063273eb87e84c6c8a03b5622323611a35586064
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-08-10 01:05:30 -0400

    cmactions: make it a privileged module

--- cmactions/Android.mk
+++ cmactions/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CMActions
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
