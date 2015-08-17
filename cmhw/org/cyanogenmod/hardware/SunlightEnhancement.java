b91612c5c5cb07a652bb4af6081f80c13b79f070
Alberto97 <albertop2197@gmail.com>
2015-08-17 20:46:52 +0200

    Add some CMHW interfaces
     * Everything stolen from CyanogenMod/android_device_motorola_msm8226-common
       and adapted for Osprey
     * file.te and system_server.te changes were already there
    
    Change-Id: Ied64e21251beb08cc988aa18a414b67f92a398d8
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

---
+++ cmhw/org/cyanogenmod/hardware/SunlightEnhancement.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+import org.cyanogenmod.hardware.util.FileUtils;
+
+import android.os.SystemProperties;
+
+import java.io.File;
+
+public class SunlightEnhancement {
+
+    private static String FILE_HBM = "/sys/class/graphics/fb0/hbm";
+
+    /**
+     * Whether device supports HBM
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        File f = new File(FILE_HBM);
+        return f.exists();
+    }
+
+    /**
+     * This method return the current activation status of HBM
+     *
+     * @return boolean Must be false when HBM is not supported or not activated, or
+     * the operation failed while reading the status; true in any other case.
+     */
+    public static boolean isEnabled() {
+        if (Integer.parseInt(FileUtils.readOneLine(FILE_HBM)) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This method allows to setup HBM
+     *
+     * @param status The new HBM status
+     * @return boolean Must be false if HBM is not supported or the operation
+     * failed; true in any other case.
+     */
+    public static boolean setEnabled(boolean status) {
+        if (status == true) {
+            return FileUtils.writeLine(FILE_HBM, "1");
+        } else {
+            return FileUtils.writeLine(FILE_HBM, "0");
+        }
+    }
+
+    /**
+     * Whether adaptive backlight (CABL / CABC) is required to be enabled
+     *
+     * @return boolean False if adaptive backlight is not a dependency
+     */
+    public static boolean isAdaptiveBacklightRequired() {
+        return false;
+    }
+}
