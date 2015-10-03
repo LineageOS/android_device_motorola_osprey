0a1f4253482f98913d370244f9249fcdaa7a5dd2
Alberto97 <albertop2197@gmail.com>
2015-10-03 23:44:12 +0200

    osprey: rootdir: Remove DTV init script
     * dtv-jelly isn't declared anywhere
     * After some searchs on the net, dtv-jelly service seems pointing to
       system/bin/jellyservice which we aren't providing
    
    Change-Id: I1e875a3beacf0b907757cf76b9a591dcacaa69c1

--- device.mk
+++ device.mk
@@ -167,7 +167,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.mmi.boot.sh \
-    init.mmi.dtv.sh \
     init.mmi.early_boot.sh \
     init.mmi.touch.sh \
     init.mmi.usb.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -43,14 +43,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mmi.dtv.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mmi.dtv.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.early_boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
