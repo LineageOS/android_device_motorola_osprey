5a9a721e252e8fd19c458dea91720d43fdcd4986
Konsta <konsta09@gmail.com>
2015-10-20 15:36:10 +0200

    osprey: fm: Update for M
    Change-Id: Idce3605937e648bc52dd41ccb5fbb03ad9e68fc4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,6 @@ TARGET_KERNEL_CONFIG := osprey_defconfig
 TARGET_OTA_ASSERT_DEVICE := osprey,osprey_umts,osprey_u2,osprey_ud2,osprey_uds,osprey_cdma,osprey_udstv,xt1540,xt1541,xt1542,xt1543,xt1544,xt1548,xt1550
 
 # Audio
-AUDIO_FEATURE_ENABLED_FM := true
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
 AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
 BOARD_USES_ALSA_AUDIO := true
