92f5668a4391b177d93085f4fe50334af21481e7
Konsta <konsta09@gmail.com>
2015-10-20 15:36:10 +0200

    osprey: fm: Update for M
    * This is a combination of following commits:
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=f20579727e984b3812232d6ff5e3d8359d8dfbab
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=6cc2ced119bb7680ad68eb61c00e87c7308c943f
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=b1d54f640c180620427bea58c07a3fb6b647746e
      https://www.codeaurora.org/cgit/quic/la/device/qcom/common/commit/?h=LA.BF.1.1.3_rb1.2&id=811a1b1fe28b57de08c52ec7f13e4d7534b4e906
      https://www.codeaurora.org/cgit/quic/la/platform/vendor/qcom/msm8916/commit/?h=LA.BR.1.2.6-00110-8x16.0&id=454ac61a5d6072321c2d3bce5ff3329f70b13ead
    
    Change-Id: Idce3605937e648bc52dd41ccb5fbb03ad9e68fc4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,6 +10,13 @@ LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.fm.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.fm.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.ril.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
