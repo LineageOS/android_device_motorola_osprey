70025a5c58d5a2ef71f785fba1620700de387651
Konsta <konsta09@gmail.com>
2015-10-20 15:36:10 +0200

    osprey: fm: Update for M
    Change-Id: Idce3605937e648bc52dd41ccb5fbb03ad9e68fc4

--- device.mk
+++ device.mk
@@ -102,10 +102,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # GPS
 PRODUCT_PACKAGES += \
