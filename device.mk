81fcf6e6eca1f4ace1caaf0c72c1338ec1fcbc83
Alberto97 <albertop2197@gmail.com>
2015-10-31 15:40:01 +0100

    osprey: Don't build Camera HAL
     * Currently we have a prebuilt one
    
    Change-Id: Ia3ae36a33221574d46965be6de3da1394fe35ec3
    (cherry picked from commit c1b193fcffb43f6ec8f0e7509b08461ce2597020)

--- device.mk
+++ device.mk
@@ -71,10 +71,6 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8916
-
 # Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
