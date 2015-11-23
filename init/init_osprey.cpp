ed22c70906707f312139a6a6aee796127d1539f3
Alberto97 <albertop2197@gmail.com>
2015-11-23 16:19:40 +0100

    osprey: init: Support for Virgin Mobile
    Change-Id: I8bd18a805d7b2ca6b207664a4eb520e127057713

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -47,6 +47,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 {
     char platform[PROP_VALUE_MAX];
     char sku[PROP_VALUE_MAX];
+    char carrier[PROP_VALUE_MAX];
     char device[PROP_VALUE_MAX];
     char devicename[PROP_VALUE_MAX];
     int rc;
@@ -60,6 +61,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         return;
 
     property_get("ro.boot.hardware.sku", sku);
+    property_get("ro.boot.carrier", carrier);
 
     property_set("ro.product.model", "MotoG3");
     property_set("ro.telephony.default_network", "9");
@@ -120,27 +122,41 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("persist.radio.process_sups_ind", "1");
     } else if (ISMATCH(sku, "XT1548")) {
         /* XT1548 */
-        property_set("ro.product.name", "osprey_usc");
+        if (ISMATCH(carrier, "sprint")) {
+            property_set("ro.product.name", "osprey_sprint");
+            property_set("ro.build.description", "osprey_sprint-user 5.1.1 LPI23.72-32-S.28.5 6 release-keys");
+            property_set("ro.build.fingerprint", "motorola/osprey_sprint/osprey_cdma:5.1.1/LPI23.72-32-S.28.5/6:user/release-keys");
+            property_set("ro.mot.build.customerid", "sprint");
+            property_set("ro.cdma.home.operator.alpha", "Virgin Mobile US");
+            property_set("ro.cdma.home.operator.numeric", "311490");
+            property_set("ril.subscription.types", "NV,RUIM");
+            property_set("ro.fsg-id", "sprint");
+        } else /*if (ISMATCH(carrier, "usc"))*/ {
+            property_set("ro.product.name", "osprey_usc");
+            property_set("ro.build.description", "osprey_usc-user 5.1.1 LPI23.72-33.2 2 release-keys");
+            property_set("ro.build.fingerprint", "motorola/osprey_usc/osprey_cdma:5.1.1/LPI23.72-33.2/2:user/release-keys");
+            property_set("ro.mot.build.customerid", "usc");
+            property_set("ro.cdma.home.operator.alpha", "U.S. Cellular");
+            property_set("ro.cdma.home.operator.numeric", "311220");
+            property_set("gsm.sim.operator.numeric", "311580");
+            property_set("ril.subscription.types", "RUIM");
+            property_set("ro.fsg-id", "usc");
+        }
         property_set("ro.product.device", "osprey_cdma");
-        property_set("ro.build.description", "osprey_usc-user 5.1.1 LPI23.72-33.2 2 release-keys");
-        property_set("ro.build.fingerprint", "motorola/osprey_usc/osprey_cdma:5.1.1/LPI23.72-33.2/2:user/release-keys");
         property_set("ro.build.product", "osprey_cdma");
-        property_set("ro.mot.build.customerid", "usc");
-        property_set("gsm.sim.operator.numeric", "311580");
         property_set("persist.radio.mode_pref_nv10", "1");
         property_set("persist.radio.0x9e_not_callname", "1");
-        property_set("ril.subscription.types", "RUIM");
         property_set("ro.cdma.data_retry_config", "max_retries=infinite,0,0,10000,10000,100000,10000,10000,10000,10000,140000,540000,960000");
         property_set("ro.cdma.ecmexittimer", "600000");
-        property_set("ro.cdma.home.operator.numeric", "311220");
-        property_set("ro.cdma.home.operator.alpha", "U.S. Cellular");
         property_set("ro.cdma.subscription", "0");
-        property_set("ro.fsg-id", "usc");
         property_set("ro.mot.ignore_csim_appid", "true");
+        property_set("ro.ril.force_eri_from_xml", "true");
         property_set("ro.ril.svlte1x", "false");
         property_set("ro.ril.svdo", "false");
         property_set("ro.telephony.default_network", "8");
+        property_set("ro.telephony.get_imsi_from_sim", "true");
         property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("telephony.slteOnCdmaDevice", "1");
     } else if (ISMATCH(sku, "XT1550")) {
         /* XT1550 */
         setMsim();
