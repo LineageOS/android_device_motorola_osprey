373e0d3ea5151aaf7aa28070a15f69d0b721ee4a
Alberto97 <albertop2197@gmail.com>
2015-11-23 16:19:40 +0100

    osprey: init: Support for Virgin Mobile
    Change-Id: I8bd18a805d7b2ca6b207664a4eb520e127057713

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ include device/motorola/msm8916-common/BoardConfigCommon.mk
 DEVICE_PATH := device/motorola/osprey
 
 # Asserts
-TARGET_OTA_ASSERT_DEVICE := osprey,osprey_umts,osprey_u2,osprey_ud2,osprey_uds,osprey_cdma,osprey_udstv,xt1540,xt1541,xt1542,xt1543,xt1544,xt1548,xt1550
+TARGET_OTA_ASSERT_DEVICE := osprey,osprey_umts,osprey_u2,osprey_ud2,osprey_uds,osprey_cdma,osprey_sprint,osprey_udstv
 
 # Init
 TARGET_UNIFIED_DEVICE := true
