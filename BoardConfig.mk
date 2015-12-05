54304cecb7164ee4acc27ca8c5f8473356c5b446
boswelja <boswelja@outlook.com>
2015-12-05 13:16:11 +1300

    Begin support for ARM64
    Change-Id: Ic1485021fcff654d554e9b23186e67258fddd197

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,9 +31,9 @@ TARGET_NO_BOOTLOADER := true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
 
 # Kernel
