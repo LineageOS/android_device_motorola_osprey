78c0c08f1c1a610c12f130f3d5af4de3c02b9ee1
Alberto97 <albertop2197@gmail.com>
2015-12-19 17:21:46 +0100

    osprey: init: Setting ro.product.name fails at boot, remove it
    Change-Id: I72159f5569761598584ad6ba9bbd202f70c63272

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -69,7 +69,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     if (ISMATCH(sku, "XT1540")) {
         /* XT1540 */
-        property_set("ro.product.name", "osprey_retus");
         property_set("ro.product.device", "osprey_umts");
         property_set("ro.build.description", "osprey_retus-user 5.1.1 LPI23.72-16.3 3 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_retus/osprey_umts:5.1.1/LPI23.72-16.3/3:user/release-keys");
@@ -78,7 +77,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.gsm.data_retry_config", "");
     } else if (ISMATCH(sku, "XT1541")) {
         /* XT1541 */
-        property_set("ro.product.name", "osprey_reteu");
         property_set("ro.product.device", "osprey_umts");
         property_set("ro.build.description", "osprey_reteu-user 5.1.1 LPI23.72-22 23 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_reteu/osprey_umts:5.1.1/LPI23.72-22/23:user/release-keys");
@@ -89,7 +87,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("persist.radio.process_sups_ind", "0");
     } else if (ISMATCH(sku, "XT1542")) {
         /* XT1542 */
-        property_set("ro.product.name", "osprey_retla");
         property_set("ro.product.device", "osprey_umts");
         property_set("ro.build.description", "osprey_retla-user 5.1.1 LPI23.72-16.4 4 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_retla/osprey_umts:5.1.1/LPI23.72-16.4/4:user/release-keys");
@@ -100,7 +97,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     } else if (ISMATCH(sku, "XT1543") || ISMATCH(radio, "0x6")) {
         /* XT1543 */
         setMsim();
-        property_set("ro.product.name", "osprey_retla_ds");
         property_set("ro.product.device", "osprey_uds");
         property_set("ro.build.description", "osprey_retla_ds-user 5.1.1 LPI23.72-9 10 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_retla_ds/osprey_uds:5.1.1/LPI23.72-9/10:user/release-keys");
@@ -112,7 +108,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     } else if (ISMATCH(sku, "XT1544")) {
         /* XT1544 */
         setMsim();
-        property_set("ro.product.name", "osprey_retbr_dstv");
         property_set("ro.product.device", "osprey_udstv");
         property_set("ro.build.description", "osprey_retbr_dstv-user 5.1.1 LPI23.72-16.4 4 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_retbr_dstv/osprey_udstv:5.1.1/LPI23.72-16.4/4:user/release-keys");
@@ -122,7 +117,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("persist.radio.process_sups_ind", "1");
     } else if (ISMATCH(sku, "XT1548")) {
         /* XT1548 */
-        property_set("ro.product.name", "osprey_usc");
         property_set("ro.product.device", "osprey_cdma");
         property_set("ro.build.description", "osprey_usc-user 5.1.1 LPI23.72-33.2 2 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_usc/osprey_cdma:5.1.1/LPI23.72-33.2/2:user/release-keys");
@@ -146,7 +140,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     } else if (ISMATCH(sku, "XT1550")) {
         /* XT1550 */
         setMsim();
-        property_set("ro.product.name", "osprey_retasia_ds");
         property_set("ro.product.device", "osprey_uds");
         property_set("ro.build.description", "osprey_retasia_ds-user 5.1.1 LPI23.72-16.3 3 release-keys");
         property_set("ro.build.fingerprint", "motorola/osprey_retasia_ds/osprey_uds:5.1.1/LPI23.72-16.3/3:user/release-keys");
