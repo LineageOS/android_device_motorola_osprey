7fe2355e38f2ef71ca1dbc07ea7412d6c10887aa
Sultan Qasim Khan <sultanqasim@gmail.com>
2015-12-25 00:44:05 -0500

    osprey: device specific media_codecs_performance
    Change-Id: Iec94b27bffce0e75a785676fe734fca607ce098e

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # Overlay
