53a4f3e0bc75831a0fc5a68278d46274a743e84f
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-21 02:57:44 +0200

    osprey: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -34,6 +34,19 @@
 #include "log.h"
 #include "util.h"
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 int is2GB()
 {
