a0af206264176b61dd5864f5f630dd481020fdc4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-01-08 07:24:01 +0530

    UBERTC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,13 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_osprey
 # Kernel
 TARGET_KERNEL_CONFIG := osprey_defconfig
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.9-uber
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456   # 262144 * 1024 mmcblk0p40
