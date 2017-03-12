9964b29a5ad7db0e1c30ed4b3cbb6ca11736e1f4
Alberto97 <albertop2197@gmail.com>
2017-03-12 15:57:15 +0100

    osprey: Set INITIAL_COPYRIGHT_YEAR
    Change-Id: I291dafccb97d4aec102f6caffc954681a712d94f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +17,8 @@
 #
 
 set -e
+
+export DEVICE_BRINGUP_YEAR=2015
 
 # Required!
 export DEVICE=osprey
