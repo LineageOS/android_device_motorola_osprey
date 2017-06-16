35c82fca43fcfaf9b57109e553b04395d1507d2a
Bruno Martins <bgcngm@gmail.com>
2017-06-16 14:35:54 +0100

    osprey: Abandon TARGET_UNIFIED_DEVICE flag
     * Override the unified properties using vendor init
    
    Change-Id: Id04671dfa32c5480000dcbe4e2d9aae3a81402b9

--- init/init_osprey.cpp
+++ init/init_osprey.cpp
@@ -176,9 +176,9 @@ void vendor_load_properties()
     sprintf(description, "osprey_%s%s%s%s-user 6.0.1 MPI24.107-55 33 release-keys", customerid, ds, tv, gb);
     sprintf(fingerprint, "motorola/osprey_%s%s%s%s/%s:6.0.1/MPI24.107-55/33:user/release-keys", customerid, ds, tv, gb, device);
 
-    property_set("ro.product.device", device);
-    property_set("ro.build.product", device);
-    property_set("ro.build.description", description);
-    property_set("ro.build.fingerprint", fingerprint);
+    property_override("ro.product.device", device);
+    property_override("ro.build.product", device);
+    property_override("ro.build.description", description);
+    property_override("ro.build.fingerprint", fingerprint);
     property_set("ro.mot.build.customerid", customerid);
 }
