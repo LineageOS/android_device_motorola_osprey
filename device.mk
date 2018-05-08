28887a4843f74031c8849a7cd18edf5cc2ee550b
Kyle Harrison <khwebmail@gmail.com>
2018-05-08 12:49:58 +0100

    media: Osprey should use the lower-end Google codecs
    Change-Id: I904ed5b9381887bb6337fdf9dbbf291073bcd6f3

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
