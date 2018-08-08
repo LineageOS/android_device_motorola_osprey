f6461a3199369eb12eaa81d75f39d8c581306a41
Sultan Qasim Khan <sultanqasim@lineageos.org>
2018-08-08 01:56:50 -0400

    osprey: use legacy camera
    moved here from common
    
    Change-Id: I38b83d14f77556dc04507ab8f8b4b00fd2b67850

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2015-2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,6 +29,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_osprey
 
 # Camera
 BOARD_GLOBAL_CFLAGS += -DCONFIG_MSM_USES_M_STACK
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
 # Kernel
 TARGET_KERNEL_CONFIG := osprey_defconfig
